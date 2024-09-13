0ef2eaa07a26ac23f135c5074134512b5cd9cd66
santiskap <santiagotula1@gmail.com>
2024-09-13 12:24:07 -0300

    Actualización de archivos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="bg-gray-800">{children}</body>
     </html>
   )
 }
